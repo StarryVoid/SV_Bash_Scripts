c706968cfb82b51908b4c3802d0ad1b722a37198
StarryVoid <starrysky806@gmail.com>
2021-04-08 16:39:12 +0800

    修改输出日志携带时间戳

--- cloudflare-ddns.sh
+++ cloudflare-ddns.sh
@@ -6,7 +6,7 @@ export PATH
 #
 # Author: StarryVoid <stars@starryvoid.com>
 # Intro:  https://blog.starryvoid.com/archives/313.html
-# Build:  2021/04/08 Version 2.3.1.2
+# Build:  2021/04/08 Version 2.3.2
 #
 
 # Select API(1) Or Token(2)
@@ -29,7 +29,7 @@ OUTPUTLOG="$(pwd)/${DOMAINNAME}.log"
 OUTPUTINFO="/tmp/ddns_${DOMAINNAME}.info"
 
 # Time
-DATETIME=$(date +%Y-%m-%d_%H-%M-%S)
+DATETIME=$(date +%Y%m%d_%H%M%S)
 
 # ------------ Start ------------
 
@@ -41,24 +41,24 @@ function check_file_directory() {
       if ! [ -f "$(pwd)/ddns_readme.log" ]; then echo "[Warning] The current directory is \"""$(pwd)""\". For management reasons, the log file path has been moved to \"/var/log/ddns/\". Remember to delete the log file similar to \"ddns.example.domain.log\"." > "$(pwd)"./ddns_readme.log ; fi
       OUTPUTLOG="/var/log/ddns/${DOMAINNAME}.log"
       if ! [ -d "/var/log/ddns/" ]; then mkdir "/var/log/ddns/" && touch "${OUTPUTLOG}" ; fi
-      if ! [ -f "${OUTPUTLOG}" ]; then echo "[Error] Could not create log file \"""${OUTPUTLOG}""\"" ; exit 1 ; fi
+      if ! [ -f "${OUTPUTLOG}" ]; then echo "[Error][${DATETIME}] Could not create log file \"""${OUTPUTLOG}""\"" ; exit 1 ; fi
     fi
   fi 
 }
 
 function check_environment () {
-  if ! [ "$(command -v pwd)" ]; then echo "[Error] Command not found \"pwd\"" >> "${OUTPUTLOG}" ; exit 1 ; fi
-  if ! [ -x "$(command -v curl)" ]; then echo "[Error] Command not found \"curl\"" >> "${OUTPUTLOG}" ; exit 1 ; fi
-  if ! [ -x "$(command -v ipcalc)" ]; then echo "[Error] Command not found \"ipcalc\"" >> "${OUTPUTLOG}" ; exit 1 ; fi
+  if ! [ "$(command -v pwd)" ]; then echo "[Error][${DATETIME}] Command not found \"pwd\"" >> "${OUTPUTLOG}" ; exit 1 ; fi
+  if ! [ -x "$(command -v curl)" ]; then echo "[Error][${DATETIME}] Command not found \"curl\"" >> "${OUTPUTLOG}" ; exit 1 ; fi
+  if ! [ -x "$(command -v ipcalc)" ]; then echo "[Error][${DATETIME}] Command not found \"ipcalc\"" >> "${OUTPUTLOG}" ; exit 1 ; fi
 }
 
 function check_selectAT () {
-  if [[ ! "${SelectAT}" = 1 && ! "${SelectAT}" = 2 ]]; then echo "[Error] Failed to Select API(1) Or Token(2), Please check the configuration." >> "${OUTPUTLOG}"; exit 1; fi
+  if [[ ! "${SelectAT}" = 1 && ! "${SelectAT}" = 2 ]]; then echo "[Error][${DATETIME}] Failed to Select API(1) Or Token(2), Please check the configuration." >> "${OUTPUTLOG}"; exit 1; fi
 }
 
 function check_ipaddress() {
-    CHECKIPADD=$1
-    if ipcalc -cs "${CHECKIPADD}" ; then echo "${CHECKIPADD}" ; fi
+  CHECKIPADD=$1
+  if ipcalc -cs "${CHECKIPADD}" ; then echo "${CHECKIPADD}" ; fi
 }
 
 function cloudflare_return_log_check() {
@@ -68,48 +68,48 @@ function cloudflare_return_log_check() {
 
 function get_cloudflare_ipaddress_api() {
   [ -z "${Data_zone_records}" ] && LOG_get_zone_records_api=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones?name=$1" -H "X-Auth-Email: ${XAUTHEMAIL}" -H "X-Auth-Key: ${XAUTHKEY}" -H "Content-Type: application/json" --connect-timeout 30 -m 10 )
-  [ -z "${Data_zone_records}" ] && if [ ! "$(cloudflare_return_log_check "${LOG_get_zone_records_api}" "success")" == "true" ]; then echo "[Error] Failed to get cloudflare \"$1\" zone_records information" >> "${OUTPUTLOG}" ; exit 1; fi
+  [ -z "${Data_zone_records}" ] && if [ ! "$(cloudflare_return_log_check "${LOG_get_zone_records_api}" "success")" == "true" ]; then echo "[Error][${DATETIME}] Failed to get cloudflare \"$1\" zone_records information" >> "${OUTPUTLOG}" ; exit 1; fi
   [ -z "${Data_zone_records}" ] && Data_zone_records=$(cloudflare_return_log_check "${LOG_get_zone_records_api}" "id")
   [ -z "${Data_dns_records}" ] && LOG_get_dns_records_api=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones/${Data_zone_records}/dns_records?type=A&name=$2" -H "X-Auth-Email: ${XAUTHEMAIL}" -H "X-Auth-Key: ${XAUTHKEY}" -H "Content-Type: application/json" --connect-timeout 30 -m 10 )
-  [ -z "${Data_dns_records}" ] && if [ ! "$(cloudflare_return_log_check "${LOG_get_dns_records_api}" "success")" == "true" ]; then echo "[Error] Failed to get cloudflare \"$2\" dns_records information" >> "${OUTPUTLOG}" ; exit 1; fi
+  [ -z "${Data_dns_records}" ] && if [ ! "$(cloudflare_return_log_check "${LOG_get_dns_records_api}" "success")" == "true" ]; then echo "[Error][${DATETIME}] Failed to get cloudflare \"$2\" dns_records information" >> "${OUTPUTLOG}" ; exit 1; fi
   [ -z "${Data_dns_records}" ] && Data_dns_records=$(cloudflare_return_log_check "${LOG_get_dns_records_api}" "id")
   LOG_get_domain_ip_api=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones/${Data_zone_records}/dns_records/${Data_dns_records}" -H "X-Auth-Email: ${XAUTHEMAIL}" -H "X-Auth-Key: ${XAUTHKEY}" -H "Content-Type: application/json" --connect-timeout 30 -m 10 )
-  if [ ! "$(cloudflare_return_log_check "${LOG_get_domain_ip_api}" "success")" == "true" ]; then echo "[Error] Failed to get cloudflare \"$2\" ip_address information" >> "${OUTPUTLOG}" ; exit 1; fi
+  if [ ! "$(cloudflare_return_log_check "${LOG_get_domain_ip_api}" "success")" == "true" ]; then echo "[Error][${DATETIME}] Failed to get cloudflare \"$2\" ip_address information" >> "${OUTPUTLOG}" ; exit 1; fi
   Data_domain_ip=$(cloudflare_return_log_check "${LOG_get_domain_ip_api}" content)
 }
 
 function get_cloudflare_ipaddress_token() {
   [ -z "${Data_zone_records}" ] && LOG_get_zone_records_token=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones?name=$1" -H "Authorization: Bearer ${AuthorizationToken}" -H "Content-Type: application/json" --connect-timeout 30 -m 10 )
-  [ -z "${Data_zone_records}" ] && if [ ! "$(cloudflare_return_log_check "${LOG_get_zone_records_token}" "success")" == "true" ]; then echo "[Error] Failed to get cloudflare \"$1\" zone_records information" >> "${OUTPUTLOG}" ; exit 1; fi
+  [ -z "${Data_zone_records}" ] && if [ ! "$(cloudflare_return_log_check "${LOG_get_zone_records_token}" "success")" == "true" ]; then echo "[Error][${DATETIME}] Failed to get cloudflare \"$1\" zone_records information" >> "${OUTPUTLOG}" ; exit 1; fi
   [ -z "${Data_zone_records}" ] && Data_zone_records=$(cloudflare_return_log_check "${LOG_get_zone_records_token}" "id")
   [ -z "${Data_dns_records}" ] && LOG_get_dns_records_token=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones/${Data_zone_records}/dns_records?type=A&name=$2" -H "Authorization: Bearer ${AuthorizationToken}" -H "Content-Type: application/json" --connect-timeout 30 -m 10 )
-  [ -z "${Data_dns_records}" ] && if [ ! "$(cloudflare_return_log_check "${LOG_get_dns_records_token}" "success")" == "true" ]; then echo "[Error] Failed to get cloudflare \"$2\" dns_records information" >> "${OUTPUTLOG}" ; exit 1; fi
+  [ -z "${Data_dns_records}" ] && if [ ! "$(cloudflare_return_log_check "${LOG_get_dns_records_token}" "success")" == "true" ]; then echo "[Error][${DATETIME}] Failed to get cloudflare \"$2\" dns_records information" >> "${OUTPUTLOG}" ; exit 1; fi
   [ -z "${Data_dns_records}" ] && Data_dns_records=$(cloudflare_return_log_check "${LOG_get_dns_records_token}" "id")
   LOG_get_domain_ip_token=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones/${Data_zone_records}/dns_records/${Data_dns_records}" -H "Authorization: Bearer ${AuthorizationToken}" -H "Content-Type: application/json" --connect-timeout 30 -m 10 )
-  if [ ! "$(cloudflare_return_log_check "${LOG_get_domain_ip_token}" "success")" == "true" ]; then echo "[Error] Failed to get cloudflare \"$2\" ip_address information" >> "${OUTPUTLOG}" ; exit 1; fi
+  if [ ! "$(cloudflare_return_log_check "${LOG_get_domain_ip_token}" "success")" == "true" ]; then echo "[Error][${DATETIME}] Failed to get cloudflare \"$2\" ip_address information" >> "${OUTPUTLOG}" ; exit 1; fi
   Data_domain_ip=$(cloudflare_return_log_check "${LOG_get_domain_ip_token}" content)
 }
 
 function get_server_new_ip() {
-    [ -z "${NEWIPADD}" ] && NEWIPADD=$( check_ipaddress "$(curl -s --retry 1 --connect-timeout 1 https://ipv4.icanhazip.com/)" )
-    [ -z "${NEWIPADD}" ] && NEWIPADD=$( check_ipaddress "$(curl -s --retry 1 --connect-timeout 1 https://api.ipify.org/)" )
-    [ -z "${NEWIPADD}" ] && NEWIPADD=$( check_ipaddress "$(curl -s --retry 1 --connect-timeout 1 https://ipinfo.io/ip/)" )
-    [ -z "${NEWIPADD}" ] && NEWIPADD=$( check_ipaddress "$(curl -s --retry 1 --connect-timeout 1 http://ipv4.icanhazip.com/)" )
-    [ -z "${NEWIPADD}" ] && NEWIPADD=$( check_ipaddress "$(curl -s --retry 1 --connect-timeout 1 http://api.ipify.org/)" )
-    [ -z "${NEWIPADD}" ] && NEWIPADD=$( check_ipaddress "$(curl -s --retry 1 --connect-timeout 1 http://ipinfo.io/ip/)" )
-    if [[ ! "${NEWIPADD}" ]]; then echo "[Error] Failed to obtain the public address of the current network." >> "${OUTPUTLOG}"; exit 1; fi
+  [ -z "${NEWIPADD}" ] && NEWIPADD=$( check_ipaddress "$(curl -s --retry 1 --connect-timeout 1 https://ipv4.icanhazip.com/)" )
+  [ -z "${NEWIPADD}" ] && NEWIPADD=$( check_ipaddress "$(curl -s --retry 1 --connect-timeout 1 https://api.ipify.org/)" )
+  [ -z "${NEWIPADD}" ] && NEWIPADD=$( check_ipaddress "$(curl -s --retry 1 --connect-timeout 1 https://ipinfo.io/ip/)" )
+  [ -z "${NEWIPADD}" ] && NEWIPADD=$( check_ipaddress "$(curl -s --retry 1 --connect-timeout 1 http://ipv4.icanhazip.com/)" )
+  [ -z "${NEWIPADD}" ] && NEWIPADD=$( check_ipaddress "$(curl -s --retry 1 --connect-timeout 1 http://api.ipify.org/)" )
+  [ -z "${NEWIPADD}" ] && NEWIPADD=$( check_ipaddress "$(curl -s --retry 1 --connect-timeout 1 http://ipinfo.io/ip/)" )
+  if [[ ! "${NEWIPADD}" ]]; then echo "[Error][${DATETIME}] Failed to obtain the public address of the current network." >> "${OUTPUTLOG}"; exit 1; fi
 }
 
 function update_new_ipaddress_api() {
-  echo "[Info] IP address will been modified from \"""${Data_domain_ip}""\" to \"""${NEWIPADD}""\"." >> "${OUTPUTLOG}"
+  echo "[Info][${DATETIME}] IP address will been modified from \"""${Data_domain_ip}""\" to \"""${NEWIPADD}""\"." >> "${OUTPUTLOG}"
   LOG_update_new_ipaddress_api=$(curl -s -X PUT "https://api.cloudflare.com/client/v4/zones/${Data_zone_records}/dns_records/${Data_dns_records}" -H "X-Auth-Email: ${XAUTHEMAIL}" -H "X-Auth-Key: ${XAUTHKEY}" -H "Content-Type: application/json"  --data "{\"type\":\"A\",\"name\":\"""$1""\",\"content\":\"""$2""\",\"ttl\":""$3"",\"proxied\":false}" --connect-timeout 30 -m 10 )
-  if [ ! "$(cloudflare_return_log_check "${LOG_update_new_ipaddress_api}" "success")" == "true" ]; then echo "[Error] Failed to update cloudflare address." >> "${OUTPUTLOG}" ; exit 1; fi
+  if [ ! "$(cloudflare_return_log_check "${LOG_update_new_ipaddress_api}" "success")" == "true" ]; then echo "[Error][${DATETIME}] Failed to update cloudflare address." >> "${OUTPUTLOG}" ; exit 1; fi
 }
 
 function update_new_ipaddress_token() {
-  echo "[Info] IP address will been modified from \"""${Data_domain_ip}""\" to \"""${NEWIPADD}""\"." >> "${OUTPUTLOG}"
+  echo "[Info][${DATETIME}] IP address will been modified from \"""${Data_domain_ip}""\" to \"""${NEWIPADD}""\"." >> "${OUTPUTLOG}"
   LOG_update_new_ipaddress_token=$(curl -s -X PUT "https://api.cloudflare.com/client/v4/zones/${Data_zone_records}/dns_records/${Data_dns_records}" -H "Authorization: Bearer ${AuthorizationToken}" -H "Content-Type: application/json"  --data "{\"type\":\"A\",\"name\":\"""$1""\",\"content\":\"""$2""\",\"ttl\":""$3"",\"proxied\":false}" --connect-timeout 30 -m 10 )
-  if [ ! "$(cloudflare_return_log_check "${LOG_update_new_ipaddress_token}" "success")" == "true" ]; then echo "[Error] Failed to update cloudflare address." >> "${OUTPUTLOG}" ; exit 1; fi
+  if [ ! "$(cloudflare_return_log_check "${LOG_update_new_ipaddress_token}" "success")" == "true" ]; then echo "[Error][${DATETIME}] Failed to update cloudflare address." >> "${OUTPUTLOG}" ; exit 1; fi
 }
 
 
@@ -121,18 +121,18 @@ function update_new_ipaddress() {
   if [ "${SelectAT}" = 1 ]; then get_cloudflare_ipaddress_api "${ZONENAME}" "${DOMAINNAME}" ; fi
   if [ "${SelectAT}" = 2 ]; then get_cloudflare_ipaddress_token "${ZONENAME}" "${DOMAINNAME}" ; fi
   if [[ "${NEWIPADD}" == "${Data_domain_ip}" ]]; then
-    echo "[Info] IP address has been modified to \"""${NEWIPADD}""\"." >> "${OUTPUTLOG}"
+    echo "[Info][${DATETIME}] IP address has been modified to \"""${NEWIPADD}""\"." >> "${OUTPUTLOG}"
     make_records_file
     exit 0
   else
-    echo "[Error] IP address modification failed." >> "${OUTPUTLOG}"
+    echo "[Error][${DATETIME}] IP address modification failed." >> "${OUTPUTLOG}"
     exit 1
   fi
 }
 
 function make_records_file() {
   echo "{\"datatime\":""${DATETIME}"",\"zone_records\":""${Data_zone_records}"",\"dns_records\":""${Data_dns_records}"",\"ip_address\":""${NEWIPADD}""}" > "${OUTPUTINFO}"
-  echo "[Info] Successfully generated DDNS information file." >> "${OUTPUTLOG}"
+  echo "[Info][${DATETIME}] Successfully generated DDNS information file." >> "${OUTPUTLOG}"
 }
 
 function read_records_file() {
@@ -142,16 +142,16 @@ function read_records_file() {
     Data_dns_records=$(cloudflare_return_log_check "${Data_file_info}" dns_records)
     Old_domain_ip=$(cloudflare_return_log_check "${Data_file_info}" ip_address)
   else
-    echo "[Warning] Could not find local configuration file." >> "${OUTPUTLOG}"
+    echo "[Alert][${DATETIME}] Could not find local configuration file." >> "${OUTPUTLOG}"
   fi
-  if ! [[ "${Data_zone_records}" && "${Data_dns_records}" && "${Old_domain_ip}" ]]; then echo "[Warning] Failed to check local configuration file." >> "${OUTPUTLOG}"; fi
+  if ! [[ "${Data_zone_records}" && "${Data_dns_records}" && "${Old_domain_ip}" ]]; then echo "[Alert][${DATETIME}] Failed to check local configuration file." >> "${OUTPUTLOG}"; fi
 }
 
 function main() {
   check_file_directory
   check_environment
   check_selectAT
-  echo "[Info] Running Time is ${DATETIME}" >> "${OUTPUTLOG}"
+#  echo "[Info][${DATETIME}] Running Time is ${DATETIME}" >> "${OUTPUTLOG}"
   get_server_new_ip
   read_records_file
   if [[ "${NEWIPADD}" != "${Old_domain_ip}" ]]; then
@@ -161,12 +161,12 @@ function main() {
       update_new_ipaddress
       exit 0
     else
-#    echo "[Info] The ip address is the same as the cloudflare record." >> "${OUTPUTLOG}"
+#    echo "[Info][${DATETIME}] The ip address is the same as the cloudflare record." >> "${OUTPUTLOG}"
       make_records_file
       exit 0
     fi
   else 
-#    echo "[Info] There is no need to change ip address." >> "${OUTPUTLOG}"
+#    echo "[Info][${DATETIME}] There is no need to change ip address." >> "${OUTPUTLOG}"
     exit 0
   fi
 }
